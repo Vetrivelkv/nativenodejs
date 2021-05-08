0cd460a1ca2262021f8d3b7bfbc46a58ea7a4c19
Vetri <37167788+Vetrivelkv@users.noreply.github.com>
2021-05-08 23:54:18 +0530

    Update sms.js

--- sms.js
+++ sms.js
@@ -5,7 +5,7 @@ const con = require('./dbconfig');
 module.exports.sendSMS = function  (otp, number){
 console.log("OTP :"+otp)
 console.log("Number :"+number)
- var options = {authorization : 'lp42wAUNCp674roaj9mMnHFcuTSW3sejGO1JqrTCTLMJAbaKhIpvuMETPzHl' , message : ''+otp ,  numbers : [number]} 
+ var options = {authorization : 'PGD7ydQsaaP645X5d7XxL5svxFBRcRhOH479AgNkINKzJYTBEFTmnSrQcDKj' , message : ''+otp ,  numbers : [number]} 
  const msg=fast2sms.sendMessage(options)
  console.log(msg)
 
