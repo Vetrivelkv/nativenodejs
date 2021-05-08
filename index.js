00aa9d89a9f53d492579c155d10f12c57ca1a05e
Vetrivelkv <vetrvelkvk@gmail.com>
2021-05-08 21:09:01 +0530

    port

--- index.js
+++ index.js
@@ -50,8 +50,10 @@ require('./Admin/route/reportRoute')(app);
 require('./Admin/route/machineRoute')(app);
 //admin route
 
+
+
 let port=process.env.PORT || '5000'
-app.listen(5000, ()=>{    
+app.listen(port, ()=>{    
 console.log('running')
  
 })
